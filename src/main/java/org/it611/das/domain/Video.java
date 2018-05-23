65dd2c60ecd7db102af9f34a4a6770a822a3d3d3
MasamiYui <546253846@qq.com>
2018-05-23 10:49:57 +0800

    数字音频/视频字段

---
+++ src/main/java/org/it611/das/domain/Video.java
@@ -0,0 +1,28 @@
+package org.it611.das.domain;
+
+public class Video {
+
+    private String id;//作品Id 资产Id
+
+    private String title;//作品名称----  歌曲：
+
+    private String des;//作品描述 ---自己描述一下这个作品
+
+    private String ownerId;//著作权人的Id（申请人）
+
+    private String author;//创作者（作者），可以是几个，用，隔开-------小明 小红
+
+    private String files;//文件url  ，隔开
+
+    private String filesHash;//文件hash  ，隔开
+
+    private String transactionId;//交易号
+
+    private String submitTime;//提交时间
+
+    private String state;//状态
+
+
+
+
+}
