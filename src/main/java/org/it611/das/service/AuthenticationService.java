be6d05ccd96b85eba3247da4f73c4a57cb2bdee0
yui <yui@linux-9cwi.lan>
2018-06-19 20:45:13 +0800

    小修改

--- src/main/java/org/it611/das/service/AuthenticationService.java
+++ src/main/java/org/it611/das/service/AuthenticationService.java
@@ -1,11 +1,21 @@
 package org.it611.das.service;
 
-import org.it611.das.vo.DrivingLicenceVO;
+import org.it611.das.vo.*;
 
 import java.util.HashMap;
 
 public interface AuthenticationService {
 
     HashMap<String, Object> authDrivingLicence(DrivingLicenceVO vo) throws Exception;
+
+    HashMap<String, Object> authDegreeCertificate(DegreeCertificateVO vo) throws Exception;
+
+    HashMap<String, Object> authSyxxzl(SyxxzlVO vo) throws Exception;
+
+    HashMap<String, Object> authAudio(MusicVO vo) throws Exception;
+
+    HashMap<String, Object> authVideo(VedioVO vo) throws Exception;
+
+    HashMap<String, Object> authImage(PhotoVO vo) throws Exception;
 
 }
